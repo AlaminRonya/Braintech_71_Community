1c128510e9db208205465481a141654b6a6cd154
AlaminRonya <hprony12@gmail.com>
2022-11-30 21:02:20 +0600

    Maximun, Minimum value and bytes

--- JavaBasic/src/main/java/com/alamin/variables/Root.java
+++ JavaBasic/src/main/java/com/alamin/variables/Root.java
@@ -25,8 +25,29 @@ public class Root {
 
         // Right shift Operator of b
         System.out.println("2 Right shift of 1 bit 0010>>0001 = "+(b>>1));
-        
+
         // Left shift Operator of b
         System.out.println("2 Left shift of 1 bit 0010>>0100 = "+(b<<1));
+
+        // Minimum and Maximum value and bytes
+        System.out.println("Byte Minimum Value: "+Byte.MIN_VALUE + " and Bytes: "+Byte.BYTES);
+        System.out.println("Byte Maximum Value: "+Byte.MAX_VALUE);
+        System.out.println("Short Minimum Value: "+Short.MIN_VALUE + " and Bytes: "+Short.BYTES);
+        System.out.println("Short Maximum Value: "+Short.MAX_VALUE);
+        System.out.println("Integer Minimum Value: "+Integer.MIN_VALUE + " and Bytes: "+Integer.BYTES);
+        System.out.println("Integer Maximum Value: "+Integer.MAX_VALUE);
+        System.out.println("Long Minimum Value: "+Long.MIN_VALUE + " and Bytes: "+Long.BYTES);
+        System.out.println("Long Maximum Value: "+Long.MAX_VALUE);
+        System.out.println("Float Minimum Value: "+Float.MIN_VALUE + " and Bytes: "+Float.BYTES);
+        System.out.println("Float Maximum Value: "+Float.MAX_VALUE);
+        System.out.println("Double Minimum Value: "+Double.MIN_VALUE + " and Bytes: "+Double.BYTES);
+        System.out.println("Double Maximum Value: "+Double.MAX_VALUE);
+
+        // Ternary operator
+        String str = "Rony";
+        String s = (str instanceof String) ? "It is an instance of String." : "It is not an instance of String.";
+        System.out.println(s);
+
+
     }
 }
